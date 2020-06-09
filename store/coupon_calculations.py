2768da12ae43446783fec047e481915f2946b43d
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-09 18:06:22 -0500

    Passed Tests
    Fixed error in test_coupon_calculations where the expected result was wrong. Changed places to 2 to stop the rounding errors that were happening.

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,5 +1,17 @@
+# constants
+TAX = .06
+
+
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    if 0 < price < 10:
+        shipping = 5.95
+        # Even if discount is greater than the price of the item customer still has to pay shipping.
+        if (price - cash_coupon)*(1-percent_coupon) < 0:
+            subtotal = shipping
+        else:
+            subtotal = (price - cash_coupon)*(1-percent_coupon)+shipping
+    total = subtotal*(1+.06)
+    return total
 
 
 if __name__ == '__main__':
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -13,12 +13,12 @@ PERCENT_COUPON_TWENTY = .2
 class MyTestCase(unittest.TestCase):
 
     def test_price_under_ten(self):
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_TEN), 8.63, places=4)
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_FIFTEEN), 8.73, places=4)
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_TWENTY), 8.84, places=4)
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_TEN), 6.31, places=4)
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_FIFTEEN), 6.31, places=4)
-        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_TWENTY), 6.31, places=4)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_TEN), 8.21, places=2)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_FIFTEEN), 8.10, places=2)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_FIVE, PERCENT_COUPON_TWENTY), 7.99, places=2)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_TEN), 6.31, places=2)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_FIFTEEN), 6.31, places=2)
+        self.assertAlmostEqual(cc.calculate_price(PRICE_UNDER_TEN, CASH_COUPON_TEN, PERCENT_COUPON_TWENTY), 6.31, places=2)
 
 
 if __name__ == '__main__':
