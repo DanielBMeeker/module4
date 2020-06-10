43b84155d6f5c9b7e5196e44b915f50a4de708f9
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-10 17:19:10 -0500

    Second test added

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -11,6 +11,8 @@ class MyTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             avg.average_try(-90, 89, 78)
+        with self.assertRaises(ValueError):
+            avg.average_try(90, -89, 78)
 
 
 if __name__ == '__main__':
